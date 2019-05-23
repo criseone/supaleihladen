16465753f009d3ea16b26416343a62d829005f10
iwant12cats <hi@alexpetercharles.ch>
2019-05-23 17:28:23 +0200

    fixed scoped styling

--- src/App.vue
+++ src/App.vue
@@ -29,21 +29,6 @@ export default {
   justify-content: center;
 }
 
-.product {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color:violet;
-  border-radius: 0.3rem;
-  padding: 1rem;
-  color: #fff;
-  margin-bottom: 1rem;
-  opacity: 1;
-  transition: opacity 0.3s ease-in-out;
-  width: 10rem;
-}
-
 button {
 display:inline-block;
 padding:0.35em 1.2em;
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -20,3 +20,20 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .product {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color:violet;
+  border-radius: 0.3rem;
+  padding: 1rem;
+  color: #fff;
+  margin-bottom: 1rem;
+  opacity: 1;
+  transition: opacity 0.3s ease-in-out;
+  width: 10rem;
+}
+</style>
