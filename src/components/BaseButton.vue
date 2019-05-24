fd6ae07939401b1318c338e4832bc50e73c620b4
Tim Fuchs <criseone@me.com>
2019-05-24 02:50:45 +0200

    added content and styling

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -24,20 +24,22 @@ export default {
 button {
 display:inline-block;
 padding:0.35em 1.2em;
-border:0.1em solid #FFFFFF;
+border:0.1em solid $button-color-1;
 margin:0 0.3em 0.3em 0;
 box-sizing: border-box;
 text-decoration:none;
-font-family:'Roboto',sans-serif;
+font-family: $primary-font;
 font-weight:300;
 color:black;
 text-align:center;
 transition: all 0.2s;
 background-color: $button-color-1;
+text-transform: uppercase;
 }
 
 button:hover{
 color: black;
+border:0.1em solid $button-color-2;
 background-color: $button-color-2;
 }
 
--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="product" :class="ProductStyle">
-        <p>{{ Product.descr }}</p>
+        <img src="../assets/placeholder_image.png" alt="Placeholder Image">
+        <p class="descr">{{ Product.descr }}</p>
+        <p class="brand">{{ Product.brand }}</p>
         <Button :onClick="rentProduct" :Product="Product" :description="ButtonDescription" />
     </div>
 </template>
@@ -32,24 +34,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../style/constants';
+@import '../style/_constants';
+
+.wrapper {
+  display: flex;
+  flex-flow: row wrap;
+}
+
+/* We tell all items to be 100% width, via flex-basis */
+.wrapper > * {
+  flex: 1 100%;
+}
+
+/* We rely on source order for mobile-first approach
+ * in this case:
+ * 1. header
+ * 2. article
+ * 3. aside 1
+ * 4. aside 2
+ * 5. footer
+ */
+
+/* All screens */
+@media all and (min-width: 800px) {
+  /* We invert order of first sidebar and main
+   * And tell the main element to take twice as much width as the other two sidebars
+   */
+  .main { flex: 2 0px; }
+  .aside-1 { order: 1; }
+  .main    { order: 2; }
+  .aside-2 { order: 3; }
+  .footer  { order: 4; }
+}
 
   .product {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #f1f1f1;
+  background-color: $secondary-color;
   padding: 1rem;
-  color: $primary-color;
   margin-bottom: 1rem;
   opacity: 1;
   transition: opacity 0.3s ease-in-out;
-  width: 10rem;
+  width: 30vmin;
+  font-family: $primary-font;
+  text-transform: uppercase;
+  }
+
+  .descr {
+    color: $primary-color;
+  }
+
+  .brand {
+    color: $tertiary-color;
   }
 
   .available {
     color: black;
     background-color: white;
   }
+
+  img {
+    width: 7vmin;
+  }
+
 </style>
